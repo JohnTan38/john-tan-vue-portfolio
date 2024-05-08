42d4ef667e8511ae9ff5c95c6cdee8ed473bd8bb
JohnTan38 <113171463+JohnTan38@users.noreply.github.com>
2024-05-08 16:57:14 +0800

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,12 +6,10 @@ import BasePage from "../components/BasePage.vue"
   <BasePage>
     <h1>About me</h1>
     <p>
-      I am a trained anthropologist who learned to code in 2020. I was hooked from the start and now
-      work as a full stack developer. While I love design and enjoy creative, responsive frontends,
-      I've discovered that my true passion is the backend: querying databases, using APIs,
-      automating processes. My projects encompass a bit of both worlds, but - apart from a small
-      site about coffee with a nice parallax effect (hopefully) that I plan to build - there will be
-      mostly data-related projects in the future.
+      I was a career banker who transitioned to AI / ML in 2015. I was hooked from the start and now
+      work as a full time data analyst and hobby full stack developer. While I love design and enjoy creative, responsive frontends,
+      I've discovered that my true passion is the backend: querying databases, using APIs, automating business processes.
+      My projects encompass a bit of both worlds, but there will be mostly data-related projects in the future.
     </p></BasePage
   >
 </template>
